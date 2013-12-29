91c48af69688831cd91bfa870d0afeb07f2f089b
Felix Meulenkamp <felix.meulenkamp@7p-group.com>
2013-12-29 20:47:50 +0100

    Add some Logging for build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 sudo rm -f *.iso
-sudo livecd-creator --verbose --config=kickstart/grist.ks --cache=cache
+sudo livecd-creator --verbose --config=kickstart/grist.ks --cache=cache --logfile=livecd.creator.log 2>&1 |tee build.log
